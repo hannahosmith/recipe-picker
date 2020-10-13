4539b5ff5a6008f0c32e984d4583d69c4e05a480
hannahsmith <fueledbyskwinklotez@gmail.com>
2020-10-12 21:27:30 -0700

    Separates recipe printing function into a separate code module

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,23 +1,20 @@
 # # require "what_should_i_eat/version"
-# # require 'what_should_i_eat/printing'
+require 'what_should_i_eat:printing'
 require 'http'
 require 'JSON'
 
 module WhatShouldIEat
-  class Error < StandardError; end
-  # Your code goes here...
+  
   def WhatShouldIEat.recipe_get
-    # self.get
-    #   puts "go ahead, ask me: \"what should i eat today?\""
-    # end
+
+    # puts "go ahead, ask me: \"what should i eat today?\""
+
     result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
     recipes = JSON.parse(result)
     recipe_name = recipes.first['items'].first['label']
     recipe_url = recipes.first['items'].first['url']
 
-    puts recipe_name
-    puts recipe_url
-    # Printing.print(recipe_name,recipe_url)
+    Printing.print recipe_name, recipe_url
 
   end
 
--- lib/what_should_i_eat:printing.rb
+++ lib/what_should_i_eat:printing.rb
@@ -1,6 +1,6 @@
 module WhatShouldIEat
   module Printing
-    def Printing.print (recipe_name, recipe_url)
+    def Printing.print(recipe_name, recipe_url)
       puts recipe_name
       puts recipe_url
     end
